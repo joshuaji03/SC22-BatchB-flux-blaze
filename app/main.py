e0b36e3db516a2b54dd29bd4e509e4736509f362
Joshua Ji <yixuaj@uw.edu>
2022-07-18 18:42:09 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -131,5 +131,6 @@ if __name__ == '__main__':
     # IMPORTANT: change url to the site where you are editing this file.
     website_url = 'cocalc11.ai-camp.dev'
     
-    print(f'Try to open\n\n    https://{website_url}' + base_url + '\n\n')
-    app.run(host = '0.0.0.0', port=port, debug=True)
+    #print(f'Try to open\n\n    https://{website_url}' + base_url + '\n\n')
+    #app.run(host = '0.0.0.0', port=port, debug=True)
+    app.run(debug=True)
